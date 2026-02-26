89ee4cde0b7c9660304be8399907c9062811afa8
lexxdaniloff <55055797+lexdanilov@users.noreply.github.com>
2026-02-26 19:31:11 +0300

    upload
    VR-Tour

---
+++ data.js
@@ -0,0 +1,227 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0----",
+      "name": "Вид с птичьего полёта",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2556559004405283,
+          "pitch": 0.11867389447562182,
+          "rotation": 0,
+          "target": "1----1"
+        },
+        {
+          "yaw": 0.47841138829867624,
+          "pitch": 0.02938447796624466,
+          "rotation": 0,
+          "target": "2----2"
+        },
+        {
+          "yaw": -0.4596331445497839,
+          "pitch": 0.19594110797111242,
+          "rotation": 0,
+          "target": "3------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1----1",
+      "name": "Вид со смотровой 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.345081263024536,
+          "pitch": -0.2731894137191375,
+          "rotation": 0,
+          "target": "0----"
+        },
+        {
+          "yaw": -0.03877627472452971,
+          "pitch": 0.31123281258187596,
+          "rotation": 0,
+          "target": "3------"
+        },
+        {
+          "yaw": 2.6641301206141605,
+          "pitch": -0.10581397085472233,
+          "rotation": 0,
+          "target": "2----2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2----2",
+      "name": "Вид со смотровой 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.200881088595942,
+          "pitch": 0.09293501053419817,
+          "rotation": 0,
+          "target": "1----1"
+        },
+        {
+          "yaw": 0.09366223325089607,
+          "pitch": 0.17397670145049204,
+          "rotation": 0,
+          "target": "3------"
+        },
+        {
+          "yaw": -0.8863706762923904,
+          "pitch": -0.15342688575703178,
+          "rotation": 0,
+          "target": "0----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3------",
+      "name": "Вид на детскую и спортивную зону",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.879881804389303,
+          "pitch": -0.415576581859753,
+          "rotation": 0,
+          "target": "1----1"
+        },
+        {
+          "yaw": 2.7403035312448365,
+          "pitch": -0.5395985349162196,
+          "rotation": 0,
+          "target": "2----2"
+        },
+        {
+          "yaw": -1.93468373060794,
+          "pitch": -0.31807210747634507,
+          "rotation": 0,
+          "target": "0----"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Чебоксары_Склон",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
